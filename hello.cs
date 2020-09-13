b5abaf93f62a6a2ad398bee187e0ac5cc5a4b339
BOXEL-REBOUND <68971688+BOXEL-REBOUND@users.noreply.github.com>
2020-09-13 11:51:16 -0400

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConsoleApp18 {
+  /* 0 references */
+  class Program {
+    /* 0 references */
+    static void Main(string[] args) {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
